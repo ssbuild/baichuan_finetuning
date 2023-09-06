0812e249a2cad0f716b35740eafcf546679b7801
ssbuild <462304@qq.com>
2023-09-07 01:20:27 +0800

    0.2.2
    Signed-off-by: ssbuild <462304@qq.com>

---
+++ module_setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# @Time:  23:34
+# @Author: tk
+# @File：model_setup
+from deep_training.utils.hf import register_transformer_model,register_transformer_config
+from config import *
+
+_model_card = train_info_args["model_name_or_path"].split('/')[-1].lower()
+if "baichuan2" in _model_card:
+    assert "7b" in _model_card or "13b" in _model_card
+    if "7b" in train_info_args["model_name_or_path"].split('/')[-1].lower():
+        from aigc_zoo.model_zoo.baichuan.baichuan2_7b.llm_model import (MyTransformer,PetlArguments, # noqa
+                                                                        LoraConfig,PetlModel,
+                                                                        PromptArguments,
+                                                                        BaichuanConfig,
+                                                                        BaichuanTokenizer)
+    else:
+        from aigc_zoo.model_zoo.baichuan.baichuan2_13b.llm_model import (MyTransformer, PetlArguments, # noqa
+                                                                        LoraConfig,PetlModel,
+                                                                        PromptArguments,
+                                                                        BaichuanConfig,
+                                                                        BaichuanTokenizer)
+else:
+    assert "7b" in _model_card or "13b" in _model_card
+    if "7b" in train_info_args["model_name_or_path"].split('/')[-1].lower():
+        from aigc_zoo.model_zoo.baichuan.baichuan_7b.llm_model import (MyTransformer, PetlArguments,  # noqa
+                                                                        LoraConfig, PetlModel,
+                                                                        PromptArguments,
+                                                                        BaichuanConfig,
+                                                                        BaichuanTokenizer)
+    else:
+        from aigc_zoo.model_zoo.baichuan.baichuan_13b.llm_model import (MyTransformer, PetlArguments,  # noqa
+                                                                         LoraConfig, PetlModel,
+                                                                         PromptArguments,
+                                                                         BaichuanConfig,
+                                                                         BaichuanTokenizer)
+
+del _model_card
+
+__all__ = [
+    "module_setup"
+]
+
+def module_setup():
+    pass
+    # 导入模型
+    #register_transformer_config(XverseConfig)
+    # register_transformer_model(LlamaForCausalLM, AutoModelForCausalLM)
